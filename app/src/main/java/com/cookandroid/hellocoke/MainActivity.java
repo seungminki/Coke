47dd0c9b09471bd234c22e4445b323c7c0920022
fsjki0328 <fsjki0328@naver.com>
2022-10-31 00:18:42 +0900

    1031

--- app/src/main/java/com/cookandroid/hellocoke/MainActivity.java
+++ app/src/main/java/com/cookandroid/hellocoke/MainActivity.java
@@ -3,8 +3,10 @@ package com.cookandroid.hellocoke;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.widget.Button;
@@ -15,36 +17,40 @@ public class MainActivity extends AppCompatActivity {
     Button button2;
     ImageView view1;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
         view1 = (ImageView) findViewById(R.id.view2);
 
+
+        //버튼 1 애니메이션
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ObjectAnimator animation = ObjectAnimator.ofFloat(view1, "translationY", -500f);
-                animation.setDuration(500);
-                animation.start();
+
+
+                ObjectAnimator anim = ObjectAnimator.ofFloat(view1, "translationY", 500f);
+                anim.setDuration(500);
+                anim.setRepeatCount(ObjectAnimator.INFINITE);
+                anim.setRepeatCount(-1);
+                anim.setRepeatCount(ObjectAnimator.REVERSE);
+
+
+
+
+                anim.start();
+
+
 
 
             }
 
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ObjectAnimator animation = ObjectAnimator.ofFloat(view1, "translationY", 500f);
-                animation.setDuration(500);
-                animation.start();
 
-
-            }
-
-        });
     }
+
 }
